42448e77dcb6d9187c89f1bc86ce2555c5402c1b
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-07-19 15:13:34 -0300

    Criação e Definição do Menu do Aplicativo

---
+++ menu.py
@@ -0,0 +1,14 @@
+
+#Criação e Definição de Menu do Aplicativo
+
+#obs 1: criação e descrição do menu (arquivo menu.py)
+#obs 2: o terminal vai rodar pelo arquivo main.py
+
+def menu_principal ():
+    print ("     FinancePy")
+    print (" 1 - Cadastrar conta: ")
+    print (" 2 - Listar contas: ")
+    print (" 3 - Marcar conta como paga: ")
+    print (" 4 - Excluir conta: ")
+    print (" 5 - Resumo Financeiro: ")
+    print (" 6 - Sair")
